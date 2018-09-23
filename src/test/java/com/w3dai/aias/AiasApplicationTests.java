7f37c5aafd466a752b88e25fd7edb4c19783b86b
Daniel Dai <335860828@qq.com>
2018-09-23 12:40:57 +0800

    Based on AuthorList file, import content to mysql database. SUCCEED!

--- src/test/java/com/w3dai/aias/AiasApplicationTests.java
+++ src/test/java/com/w3dai/aias/AiasApplicationTests.java
@@ -1,16 +1,28 @@
 package com.w3dai.aias;
 
+import com.w3dai.aias.authorInformation.AuthorInfoService;
+import com.w3dai.aias.authorInformation.entity.Author;
+import com.w3dai.aias.paperInformation.service.AuthorService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AiasApplicationTests {
 
+    @Autowired
+    AuthorInfoService authorInfoService;
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws IOException {
+        List<Author> authorList;
+        authorInfoService.importDataToMySQL();
     }
 
 }
