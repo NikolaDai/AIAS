90cae5c862e849783f5c885a704bb841217cda01
NicolaDai <335860828@qq.com>
2018-10-10 17:07:11 +0800

    Delete some useless import code line.

--- src/main/java/com/w3dai/aias/paperInformation/config/Config.java
+++ src/main/java/com/w3dai/aias/paperInformation/config/Config.java
@@ -1,12 +1,9 @@
 package com.w3dai.aias.paperInformation.config;
 
-import org.elasticsearch.action.search.SearchResponse;
-import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
 
 import java.net.InetAddress;
 
