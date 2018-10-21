3e9f89e00894882e579d13e623c1c1e3e88bfcac
NicolaDai <335860828@qq.com>
2018-10-21 23:19:48 +0800

    lots of changes will happen here.

--- src/main/java/com/w3dai/aias/config/SecurityConfiguration.java
+++ src/main/java/com/w3dai/aias/config/SecurityConfiguration.java
@@ -2,31 +2,31 @@ package com.w3dai.aias.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureAuth(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication()
-                .withUser("admin").password("admin").roles("ADMIN");
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http.httpBasic()
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.POST,"/search/**").permitAll()
-                .antMatchers(HttpMethod.GET, "/searchAuthor/**").permitAll()
-                .antMatchers(HttpMethod.POST, "/searchAuthor/**").hasRole("ADMIN")
-                .anyRequest().authenticated();
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder()).withUser("admin").password(new BCryptPasswordEncoder().encode("admin")).roles("ADMIN");
     }
 }
+
+/**
+ *     @Override
+ *     protected void configure(HttpSecurity http) throws Exception{
+ *         http.httpBasic()
+ *                 .and()
+ *                 .csrf().disable()
+ *                 .authorizeRequests()
+ *                 .antMatchers("/").permitAll()
+ *                 .anyRequest().authenticated();
+ *     }
+ *                 .antMatchers(HttpMethod.POST,"/search/**").permitAll()
+ *                 .antMatchers(HttpMethod.GET, "/searchAuthor/**").permitAll()
+ *                 .antMatchers(HttpMethod.POST, "/searchAuthor/**").hasRole("ADMIN")
+ */
